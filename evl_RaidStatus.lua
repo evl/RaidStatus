42ddc69303d4d3b5ec7c5356f0e23ade915bdf2a
Evl <wow.evl@gmail.com>
2010-08-18 17:41:00 +0200

    Fixed a small memory leak. Minor optimizations.

--- evl_RaidStatus.lua
+++ evl_RaidStatus.lua
@@ -5,7 +5,7 @@ local text = frame:CreateFontString(nil, "ARTWORK")
 text:SetFontObject(GameFontHighlightSmall)
 text:SetPoint("TOPLEFT", frame)
 
-local MemberSortCompare = function(a, b)
+local memberSortCompare = function(a, b)
 	return ((a.color.r + a.color.g + a.color.b) .. a.name) < ((b.color.r + b.color.g + b.color.b) .. b.name)
 end
 
@@ -20,15 +20,15 @@ local result, name, unit, callback, count
 local onUpdate = function(self, elapsed)
 	lastUpdate = lastUpdate + elapsed
 	
-	if lastUpdate > updateInterval then
+	if lastUpdate > updateInterval and GetNumRaidMembers() > 0 then
 		lastUpdate = 0
-		result = {}
+		result = nil
 
 		for name, callback in pairs(watches) do
 			count = 0
 			
-			for i = 1, MAX_RAID_MEMBERS do
-				local unit = "raid" .. i
+			for i = 1, GetNumRaidMembers() do
+				unit = "raid" .. i
 
 				if UnitExists(unit) and callback(unit) then
 					count = count + 1
@@ -36,28 +36,34 @@ local onUpdate = function(self, elapsed)
 			end
 			
 			if count > 0 then
-				table.insert(result, name .. ": " .. count)
+				result = (result and result .. ", " or "") .. name .. ": " .. count
 			end
 		end
 		
-		text:SetText(#result > 0 and strjoin(", ", unpack(result)) or "")
-
-		frame:SetWidth(text:GetWidth())
+		if result then
+			text:SetText(result)
+			text:Show()
+			
+			frame:SetWidth(text:GetWidth())
+		else
+			text:Hide()
+		end
 	end
 end
 
-local matches, class, classId, match
+local matches, match, class, classId
 local onEnter = function()
 	GameTooltip:SetOwner(frame, "ANCHOR_BOTTOMLEFT")
 	
 	for name, callback in pairs(watches) do
 		matches = {}
 		
-		for i = 1, MAX_RAID_MEMBERS do
-			local unit = "raid" .. i
+		for i = 1, GetNumRaidMembers() do
+			unit = "raid" .. i
 
 			if UnitExists(unit) and callback(unit) then
 				class, classId = UnitClass(unit)
+				
 				table.insert(matches, {name = UnitName(unit), color = RAID_CLASS_COLORS[classId]})
 			end
 		end
@@ -69,7 +75,7 @@ local onEnter = function()
 
 			GameTooltip:AddLine(name .. ":", 1, 1, 1)
 			
-			table.sort(matches, MemberSortCompare)
+			table.sort(matches, memberSortCompare)
 			
 			for _, match in pairs(matches) do
 				GameTooltip:AddLine(match.name, match.color.r, match.color.g, match.color.b)
@@ -85,4 +91,3 @@ frame:SetHeight(16)
 frame:SetScript("OnUpdate", onUpdate)
 frame:SetScript("OnEnter", onEnter)
 frame:SetScript("OnLeave", function() GameTooltip:Hide() end)
-
